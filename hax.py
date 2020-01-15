4851c59be00d7ee2b8fc013e426df1ced3c7474b
floppyfish14 <mfish551.mf@gmail.com>
2020-01-15 15:25:05 -0800

    changed create statement

--- hax.py
+++ hax.py
@@ -1,6 +1,5 @@
 import mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
 friendsIP = "45.33.105.164"
 friendsPort = 25565
-hackedMC = minecraft.Minecraft.create(friendsIP)
+hackedMC = minecraft.Minecraft.create(friendsIP, friendsPort)
 hackedMC.postToChat("Hello World")
