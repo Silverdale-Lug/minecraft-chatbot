36f3cd982e759fbf6fdd830ad6883d1cef6d3504
floppyfish14 <mfish551.mf@gmail.com>
2020-01-15 15:16:58 -0800

    added port number

--- hax.py
+++ hax.py
@@ -1,5 +1,6 @@
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create()
-friendsIP = ""
+friendsIP = "45.33.105.164"
+friendsPort = 25565
 hackedMC = minecraft.Minecraft.create(friendsIP)
 hackedMC.postToChat("Hello World")
