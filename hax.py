cce1aea86f6f4b7d78aa936636d9c9ebc42e30a3
vinividivici47 <vinividivici47@github.com>
2020-01-15 23:11:12 +0000

    first commit

--- hax.py
+++ hax.py
@@ -1,5 +1,5 @@
 import mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.creat()
-friendsIP = "24.16.163.30"
+mc = minecraft.Minecraft.create()
+friendsIP = ""
 hackedMC = minecraft.Minecraft.create(friendsIP)
 hackedMC.postToChat("Hello World")
