cc4959faa729f3929d1c368905e40807771cf53e
floppyfish14 <mfish551.mf@gmail.com>
2020-01-15 23:41:12 -0800

    changed a lot

--- hax.py
+++ hax.py
@@ -1,5 +1,17 @@
 import mcpi.minecraft as minecraft
-friendsIP = ""
-friendsPort = 25565
-hackedMC = minecraft.Minecraft.create(address=friendsIP, port=friendsPort)
-hackedMC.postToChat("Hello World")
+import sys
+
+def usage():
+    print("Usage: python %s IP_of_Minecraft_Server Port_of_Minecraft_Server"%sys.argv[0])
+    exit(1)
+
+if len(sys.argv) < 2:
+    usage()
+
+ip = sys.argv[1]
+port = sys.argv[2]
+
+mc = minecraft.Minecraft.create(address=str(ip), port=int(port))
+x, y, z = mc.player.getPos()
+mc.postToChat("Position: x: %f y: %f z: %f"% (x,y,z))
+print(x,y,z)
